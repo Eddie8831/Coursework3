7bbd3b1f275062ce25542bf0f2395d97767fce97
JiayiYan <eddieyan520@gmail.com>
2025-05-15 15:32:58 +0100

    Update available_img.js

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -1,1 +1,12 @@
-const availableImg = ["Beck2016Visual"];
+const availableImg = [
+  "Hammed2023AI",
+  "Ucar2024AI",
+  "Patil2024AI",
+  "Frontiers2025AI",
+  "Manufacturing2023AI",
+  "Lawrence2023AI",
+  "Poland2024AI",
+  "Malawade2021AI",
+  "Khosravi2023AI",
+  "Hamel2024AI"
+];
